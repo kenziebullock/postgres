853ddf5554087e15b854fac9d539f968ad5acea0
Kenzie Bullock <kenzieb_13@shaw.ca>
2018-09-18 20:58:40 +0000

    added migration for table milestones

---
+++ knexfile.js
@@ -0,0 +1,53 @@
+// Update with your config settings.
+const settings = require('./settings')
+
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: settings.database,
+      user:     settings.user,
+      password: settings.password
+      // filename: './settings'
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './migrations'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
